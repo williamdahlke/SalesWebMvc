1b1c6e43e0ebe36cdddc8c34f41cdd689ac49d50
William Guilherme Dahlke <williamgd@weg.net>
2024-02-08 17:35:19 -0300

    Inicializing to develop other entities

---
+++ Models/Seller.cs
@@ -0,0 +1,14 @@
+﻿namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+    }
+}
