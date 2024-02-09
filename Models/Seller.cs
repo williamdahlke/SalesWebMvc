63287953e42ff15f5add628dd3d76e46cedf262d
williamdahlke <williamgdahlke@gmail.com>
2024-02-08 22:15:06 -0300

    Finished OtherEntities creation and migration

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -10,5 +10,33 @@
         public Department Department { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
+        public Seller()
+        {
+        }
+
+        public Seller(int id, string nome, string email, DateTime birthDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+
+        public void RemoveSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+
+        public double TotalSales(DateTime inicial, DateTime final)
+        {
+            return Sales.Where(sr => sr.Date >= inicial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
     }
 }
